b282d401d0d0d91155e6f49a0c5a88b1077d4007
Eze-Ricca <riccaesteban@gmail.com>
2025-05-08 22:52:01 -0300

    modifique una variable de entonrno

--- server.js
+++ server.js
@@ -73,5 +73,5 @@ app.get("/products/:id", (req, res) => {
   res.json(productBFound);
 });
 
-app.listen(3000);
-console.log(`Server on port ${3000}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
